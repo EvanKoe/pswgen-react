8a3495d832fcf98ce9285b0ddd03ba163c8fdc60
Evan Koehler <evan.koehler@epitech.eu>
2021-09-29 16:59:08 +0200

    removed clear button (but should put it again

--- src/Generator/Gen.tsx
+++ src/Generator/Gen.tsx
@@ -135,7 +135,7 @@ const Generator = () => {
         onClick={() => gen()}
         className="button genBtn"
       />
-      <input type='button' onClick={() => localStorage.clear()} value='CLEAR' />
+	  {/* <input type='button' onClick={() => localStorage.clear()} value='CLEAR' /> */}
     </div>
   );
 };
